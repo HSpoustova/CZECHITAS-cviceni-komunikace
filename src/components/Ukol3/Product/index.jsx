16b8b136003026eae28807626a71d26ad97adb6b
Hana Spoustová <bernreih@gmail.com>
2023-12-18 23:09:32 +0100

    reseni

--- src/components/Ukol3/Product/index.jsx
+++ src/components/Ukol3/Product/index.jsx
@@ -1,10 +1,16 @@
 import './style.css';
 
-export const Product = ({ image, name, price }) => {
+export const Product = ({ image, name, price, onSelectPrice }) => {
+  const handleClick = () => {
+    onSelectPrice(price);
+  };
+
   return (
-    <div className="product">
+    <div className="product" onClick={handleClick}>
       <img src={image} className="product__image" />
-      <div className="product__body">{name}: {price} Kč</div>
+      <div className="product__body">
+        {name}: {price} Kč
+      </div>
     </div>
   );
 };
--- src/components/Ukol4/InputField/index.jsx
+++ src/components/Ukol4/InputField/index.jsx
@@ -1,12 +1,19 @@
 import './style.css';
 
 export const InputField = ({ label, type, value, onValueChange }) => {
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    onValueChange(newValue);
+  };
+
   return (
     <div className="input-field">
       <label className="input-field__label">{label}</label>
       <input
         className="input-field__input"
         type={type}
+        onChange={handleChange}
+        value={value}
       />
     </div>
   );
